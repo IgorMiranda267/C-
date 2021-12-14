36f074eb8e2b1a8cbbebe0d15875e8f65f457b9b
Igor Miranda <62434333+IgorMiranda267@users.noreply.github.com>
2021-12-14 17:02:06 -0300

    Add files via upload

---
+++ Encapsulamento -/Encapsulamento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine("Ente os dados do produto:");
+
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+
+            //Console.Write("Preço: ");
+
+            //Console.WriteLine("Dados dos produtos:");
+
+            //Console.Write("Digite o número de produtos a ser adicionados: ");
+
+            //Console.WriteLine("Dados atualizados: " + p);
+
+            //Console.Write("Digite o número de produtos a ser removidos: ");
+
+            //Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
