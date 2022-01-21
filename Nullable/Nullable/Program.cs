f703e1dd775e85390f48cbcf691e8c101e873d26
Igor Miranda <62434333+IgorMiranda267@users.noreply.github.com>
2022-01-21 14:25:54 -0300

    Add files via upload
    Nullable é um recurso de C# para que dados de tipo valor (structs) possam receber o valor null.
    Métodos usados
     GetValueOrDefault, HasValue e Value.

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // É a mesma coisa que usar " Nullable<double> x = null; ".
+            double? x = null;
+            double? y = 10.0;
+
+            //GetValueOrDefault() mostrara o valor contido na variavel, caso nao tenha, mostrará o valor padrão do tipo da variavel.
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            //HasValue mostrará verdadeiro se tiver algum valor na variavel e Falso se nao tiver.
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue) Console.WriteLine(x.Value);        
+            else Console.WriteLine("X is null");
+
+            if (y.HasValue) Console.WriteLine(y.Value);
+            else Console.WriteLine("Y is null");
+
+        }
+    }
+}
