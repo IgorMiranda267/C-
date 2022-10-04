2699693c031b5c47a711848048f80bb0af72ce89
Igor Miranda <igormiranda267@gmail.com>
2022-10-04 15:42:40 -0300

    Ordenamento Lista

---
+++ Exercicio_Lista/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Exercicio_Lista
+{
+    class Employee
+    {
+        // Atributos Privados.
+        public int Id { get; set;}
+        public string Name { get; set;}
+        public double Salary { get; private set;}
+
+        //Construtores
+        public Employee (int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        //Propriedades Autoimplementadas
+        public void SalaryIncrement( double percentage)
+        {
+            Salary += Salary * percentage / 100;
+        }
+
+        //Outros Métodos da Classe
+        public override string ToString()
+        {
+            return Id + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InstalledUICulture);
+        }
+
+    }
+}
---
+++ Exercicio_Lista/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicio_Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int num = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+            
+
+            for(int i = 1; i <= num; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i + ":");
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine("");
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int IdPesquisa = int.Parse(Console.ReadLine());
+
+
+            Employee emp = list.Find(x => x.Id == IdPesquisa);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.SalaryIncrement(percentage);
+
+            }
+            else { Console.WriteLine("This id does not exist!"); }
+
+
+            /*
+             *Ordenamento de uma lista de Employe, em ordem crescente.
+             */
+            Console.WriteLine("");
+            list = list.OrderBy(x => x.Id).ToList();
+
+
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
